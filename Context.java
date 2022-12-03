a85600ae5c305e3d665885af37e75e5f3a5047ca
zidan kharisma a <zidan.kharisma@ui.ac.id>
2022-12-03 09:31:16 +0700

    tambah rule parser

--- Context.java
+++ Context.java
@@ -195,6 +195,38 @@ class Context
                         break;
                 }
                 break;
+            case 22:
+                break;
+            case 23:
+                break;
+            case 24:
+                break;
+            case 25:
+                break;
+            case 26:
+                break;
+            case 27:
+                break;
+            case 28:
+                break;
+            case 29:
+                break;
+            case 30:
+                break;
+            case 31:
+                break;
+            case 32:
+                break;
+            case 33:
+                break;
+            case 34:
+                break;
+            case 35:
+                break;
+            case 36:
+                break;
+            case 37:
+                break;
         }
     }
 
